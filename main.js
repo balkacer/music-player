9b4d6df1ba9c34eec92bc38ea972d2194bf2950e
Enmanuel Balcacer <makuttico@gmail.com>
2021-06-20 23:29:58 -0400

    Creating progress player bar

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
 //  I'll create the logic to bring the most amazing music player to live here!
+
+documentStyle = document.documentElement.style;
+documentStyle.setProperty('--current-progress-bar-percent', '8%');
